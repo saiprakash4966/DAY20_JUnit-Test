8221839043a8377e98ba5a7939261262b68174d3
Your Name <you@example.com>
2022-03-14 21:36:11 +0530

    User Password one caps

--- main/java/com/sai/User.java
+++ main/java/com/sai/User.java
@@ -71,7 +71,7 @@ public class User
 		 */
 		public boolean checkPassword(String password)
 		{
-			return (password.matches("[a-zA-Z]{8,}"));
+			return (password.matches("^(?=.*[A-Z])(?=.*[a-z]).{8,}$"));
 		}
 	}
 
